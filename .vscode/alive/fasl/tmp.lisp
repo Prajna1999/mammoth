f1b1d1002a8cfee5a22ac23ed2b112b33ffe45b5
Prajna1999 <gituprajna20@gmail.com>
2025-06-04 21:58:59 +0530

    CHORE: add deepresearch PRD

--- .vscode/alive/fasl/tmp.lisp
+++ .vscode/alive/fasl/tmp.lisp
@@ -1,11 +1,13 @@
-(asdf:defsystem "mammoth"
+(defpackage :mammoth
 
-  :description "A mongodb like NoSQL document database written in Common Lisp"
-  :version "0.1.0"
-  :author "Prajna Prayas"
-  :license "MIT"
-  :depends-on (:alexandria :frugal-uuid)
-  :serial t
-  :components ((:file "packages")
-               (:file "main")
-               (:file "storage")))
+  (:use :cl :alexandria)
+  (:export :hello))
+
+(defpackage :mammoth.storage
+  (:use :cl :alexandria :frugal-uuid)
+  (:export :make-store :insert-document :get-document :delete-document))
+
+(defpackage :mammoth.query
+
+  (:use :cl :alexandria)
+  (:export :evaluate-query))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Mammoth is a mongodb like document store written in Common LISP. This project inspired by Elephant DB.
+
+
+
